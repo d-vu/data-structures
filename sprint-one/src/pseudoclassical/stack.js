1042bf7f035e3bd17a364ca6ae40b14a453390e5
d-vu <daniel.h.vu@gmail.com>
2016-04-21 10:25:24 -0700

    pseudo classical pattern: completed stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.size() > 0) {
+    var temp = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return temp;
+  }
+};
+
+Stack.prototype.size = function() {
+  var length = Object.keys(this.storage).length;
+  return length;
+};
+
 
 
