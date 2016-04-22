6f898e5e03fdbe596fbdd8946e69c21805e6b5dc
d-vu <daniel.h.vu@gmail.com>
2016-04-22 11:32:37 -0700

    Completed Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,25 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage === undefined) {
+    this._storage = [];
+  }
+  this._storage.push(item);
+
 };
 
 setPrototype.contains = function(item) {
+  return (this._storage.indexOf(item) !== -1);
 };
 
 setPrototype.remove = function(item) {
+  
+  if (this.contains(item)) {
+    var index = this._storage.indexOf(item);
+    this._storage.splice(index, 1);
+  }
+  
+
 };
 
 /*
